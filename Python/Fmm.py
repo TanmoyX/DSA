2c4dafc2dbb72301dff6e8910db6cd29b2781914
Tanmoy Mukherjee <tanmoymukherjee02@gmail.com>
2017-12-17 22:40:01 +0530

    Update Fmm.py
    The entire algorithm was giving wrong answer..

--- Python/Fmm.py
+++ Python/Fmm.py
@@ -2,21 +2,14 @@
 
 # Fast Modulo Multipication calcaulates (a^b)%m in log(b) time
 # It is based on the fact (a*b)%m = ((a%m)*(b%m))%m
-
-def FastMultiplication(base, power, modulo):
-    base = int(base)
-    power = int(power)
-    modulo = int(modulo)
-    result = 1
-    while power > 0:
-        if power%2 != 0:
-            result = (result * base) % modulo
-        base = (base * base) % modulo
-        power = power / 2
-    return result
-
-if __name__ == "__main__":
-    base = raw_input("Enter base > ")
-    power = raw_input("Enter power > ")
-    modulo = raw_input("Enter modulo > ")
-    print FastMultiplication(base, power, modulo)
+def fast_mul(base, exp, mod):
+    res = 1
+    while exp > 0:
+        if exp % 2 == 1:
+            res = (res*base)%mod
+        base = (base*base)%mod
+        exp //= 2
+    return res%mod
+b, e, m = map(int, input().split())
+print (fast_mul(b,e,m))
+#wrong implementation was given.. please correct before posting...
